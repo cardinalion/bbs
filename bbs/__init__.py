5487439cbdbc1d180b420969537214d2f964c48d
unknown <ywang10@wesleyan.edu>
2019-05-27 21:58:19 +0800

    init & error templates

--- bbs/__init__.py
+++ bbs/__init__.py
@@ -5,7 +5,42 @@ cardinalion
 
 from flask import Flask
 
+from bluelog.extensions import db, csrf, ckeditor, toolbar
 
-def create_app():
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG','development')
+
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    # register_logging(app)
+    register_extensions(app)
+    register_blueprints(app)
+    register_commands(app)
+    register_errors(app)
+    register_shell_context(app)
+    register_template_context(app)
+
     return app
+
+def register_extensions(app):
+    db.init_app(app)
+    ckeditor.init_app(app)
+
+def register_blueprints(app):
+    app.register_blueprint()
+    pass
+
+def register_shell_context(app):
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(db=db)
+
+def register_template_context(app):
+    pass
+
+def register_errors(app):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('errors/400.html'), 400
--- bbs/extensions.py
+++ bbs/extensions.py
@@ -3,7 +3,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_ckeditor import CKEditor
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
 toolbar = DebugToolbarExtension()
+ckeditor = CKEditor()
